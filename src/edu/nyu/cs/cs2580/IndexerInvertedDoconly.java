926c74b90f697af434398dd4a1934aae47b87c8e
thanakorntr <thanakorn@nyu.edu>
2014-10-13 13:45:22 -0400

    Implemented nextDoc method in inverteddoconly

--- src/edu/nyu/cs/cs2580/IndexerInvertedDoconly.java
+++ src/edu/nyu/cs/cs2580/IndexerInvertedDoconly.java
@@ -225,12 +225,41 @@ public class IndexerInvertedDoconly extends Indexer implements Serializable{
    * In HW2, you should be using {@link DocumentIndexed}
    */
   @Override
+  // This implementation follows that in the lecture 3 slide, page 13.
   public Document nextDoc(Query query, int docid) {
-    return null;
+	// Assuming that the query has already been processed.
+	// query.processQuery();
+	List<Integer> docIDs = new ArrayList<Integer>();  // a list containing doc ID for each term in the query
+	for(String token : query._tokens){
+		int docID = next(token,docid);
+		if(docID == -1){
+			return null;
+		}
+		docIDs.add(docID);
+	}
+	
+	boolean foundDocID = true;
+	int docIDFixed = docIDs.get(0); 
+	int docIDNew = Integer.MIN_VALUE;
+	
+	for(Integer docID : docIDs){  // check if all the doc IDs are equal
+		if(docID != docIDFixed){
+			foundDocID = false;
+		}
+		if(docID > docIDNew){
+			docIDNew = docID;
+		}
+	}
+	
+	if(foundDocID){
+		return _documents.get(docIDFixed);
+	}
+	
+    return nextDoc(query,docIDNew-1);
   }
   
   // Just like in the lecture slide, this helper method returns the next document id
-  // after the given docid
+  // after the given docid. It returns -1 if not found.
   public int next(String term, int docid){
 	  
 	  return -1;
