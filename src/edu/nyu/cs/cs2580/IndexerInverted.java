174fd8d1c6f5abc341f969c93a1231ee0c36cc9c
thanakorntr <thanakorn@nyu.edu>
2014-10-18 12:45:06 -0400

    Implemented the populateStoppingWords() method

--- src/edu/nyu/cs/cs2580/IndexerInverted.java
+++ src/edu/nyu/cs/cs2580/IndexerInverted.java
@@ -164,8 +164,38 @@ public abstract class IndexerInverted extends Indexer implements Serializable {
   
   // This method populates the top 50 most frequent words into _stoppingWords.
   private void populateStoppingWords(){
+	  Map<Integer, Integer> sortedTermCorpusFrequency = sortByValues(_termCorpusFrequency); 
 	  
+      Set termSet = sortedTermCorpusFrequency.entrySet();
+      Iterator iterator = termSet.iterator();
+      for(int i=0;i<50;i++) {  // extracting the top 50 terms (the order is preserved)
+           Map.Entry me = (Map.Entry)iterator.next();
+           int termId = (Integer)me.getKey();
+           String term = _dictionary.inverse().get(termId);
+           _stoppingWords.add(term);
+      }
   }
+  
+  // This helper method sorts the given map by value in a decreasing order.
+  private HashMap sortByValues(Map map) { 
+      List list = new LinkedList(map.entrySet());
+      // Defined Custom Comparator here
+      Collections.sort(list, new Comparator() {
+           public int compare(Object o1, Object o2) {
+              return ((Comparable) ((Map.Entry) (o2)).getValue())
+                 .compareTo(((Map.Entry) (o1)).getValue());
+           }
+      });
+
+      // Here I am copying the sorted list in HashMap
+      // using LinkedHashMap to preserve the insertion order
+      HashMap sortedHashMap = new LinkedHashMap();
+      for (Iterator it = list.iterator(); it.hasNext();) {
+             Map.Entry entry = (Map.Entry) it.next();
+             sortedHashMap.put(entry.getKey(), entry.getValue());
+      } 
+      return sortedHashMap;
+ }
 
   private void precomputeSquareTFIDFSum(Map<Integer, Vector<Integer>> docBodies) {
     // Precompute squared tfidf sum of documents.
