a2d4b6b5b25fa41be64a3a2ab4fea22c20f0b7de
thanakorntr <thanakorn@nyu.edu>
2014-11-14 12:56:55 -0500

    Set up RankerComprehensive (still need to adjust weighing parameters).

--- src/edu/nyu/cs/cs2580/RankerComprehensive.java
+++ src/edu/nyu/cs/cs2580/RankerComprehensive.java
@@ -1,5 +1,7 @@
 package edu.nyu.cs.cs2580;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Vector;
 
 import edu.nyu.cs.cs2580.QueryHandler.CgiArguments;
@@ -12,14 +14,53 @@ import edu.nyu.cs.cs2580.SearchEngine.Options;
  */
 public class RankerComprehensive extends Ranker {
 
+	// TODO: Adjust the weighing parameters below by experimentation.
+  public static final double BETA_COS = 1.0/0.8;
+  public static final double BETA_PAGERANK = 1.0/0.8;
+  public static final double BETA_NUMVIEWS = 1.0/20000.0;
+
+  RankerCosine _cosineRanker;
+	
   public RankerComprehensive(Options options,
       CgiArguments arguments, Indexer indexer) {
     super(options, arguments, indexer);
+    _cosineRanker = new RankerCosine(options, arguments, indexer);
     System.out.println("Using Ranker: " + this.getClass().getSimpleName());
   }
 
   @Override
   public Vector<ScoredDocument> runQuery(Query query, int numResults) {
-    return null;
+    Vector<ScoredDocument> results = new Vector<ScoredDocument>();
+    
+    PriorityQueue<ScoredDocument> scoredDocuments = new PriorityQueue<ScoredDocument>();
+    Document doc = _indexer.nextDoc(query, -1);
+    
+    while(doc!=null){
+    	Double score = scoreDocument(query,doc._docid);
+    	scoredDocuments.add(new ScoredDocument(doc,score));
+    	if(scoredDocuments.size()>numResults){
+    		scoredDocuments.poll();
+    	}
+    	doc = _indexer.nextDoc(query, doc._docid);
+    }
+    
+    while(!scoredDocuments.isEmpty()){
+    	results.add(scoredDocuments.poll());
+    }
+    Collections.sort(results, Collections.reverseOrder());
+    return results;
+  }
+  
+  private double scoreDocument(Query query, int did) {
+
+    double cosineScore = _cosineRanker.scoreDocument(query, did);
+    double pageRank		 = _indexer.getDoc(did).getPageRank(); 
+    double numViews    = _indexer.getDoc(did).getNumViews();
+
+    double combinedScore = BETA_COS      * cosineScore +
+    		               BETA_PAGERANK       * pageRank +
+    		               BETA_NUMVIEWS * numViews;
+
+    return combinedScore;
   }
 }
