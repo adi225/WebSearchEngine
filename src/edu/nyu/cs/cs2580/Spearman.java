1d375ffd601044b2118afb9a95a18b9efadee3a9
adi225 <adi225@nyu.edu>
2014-11-20 22:03:43 -0500

    Refactored some of the Spearman code - mostly cosmetic.

--- src/edu/nyu/cs/cs2580/Spearman.java
+++ src/edu/nyu/cs/cs2580/Spearman.java
@@ -1,5 +1,7 @@
 package edu.nyu.cs.cs2580;
 
+import com.google.common.collect.Lists;
+
 import java.io.*;
 import java.util.*;
 
@@ -15,148 +17,144 @@ import java.util.*;
 
 public class Spearman
 {
-    static String pathToPagerank = null;
-    static String pathToNumviews = null;
-    static BufferedReader br = null;
+  static String pathToPagerank = null;
+  static String pathToNumviews = null;
+  static BufferedReader br = null;
 
-    public static void main(String[] args) throws Exception {
-        if (args == null || args.length != 2)
-        {
-            System.err.println("Fatal error: needs 2 parameters");
-            System.exit(-1);
-        }
-
-        pathToPagerank = args[0];
-        pathToNumviews = args[1];
-
-        // count the number of documents we are considering
-        br = new BufferedReader(new FileReader(pathToPagerank));
-        String line = null;
-        int n = 0;
-        while ((line = br.readLine()) != null)
-        {
-            n++;
-        }
-        br.close();
-
-        String[] singleLine;
-        String doc;
-        Double[] pageRank = new Double[n];
-        Integer[] numView = new Integer[n];
-        double[] x_k, y_k;
-        List<String> docName = new ArrayList<String>();
-
-        for (int i = 0; i < n; i++)
-            numView[i] = 0;
-
-        br = new BufferedReader(new FileReader(pathToPagerank));
-        for (int i = 0; i < n; i++)
-        {
-            line = br.readLine();
-            singleLine = line.split("\\s+");
-            docName.add(singleLine[0]);
-            pageRank[i] = Double.parseDouble(singleLine[1]);
-        }
-        br.close();
-
-        br = new BufferedReader(new FileReader(pathToNumviews));
-        while ((line = br.readLine()) != null)
-        {
-            singleLine = line.split("\\s+");
-            doc = singleLine[0];
-            if (docName.contains(doc))
-            {
-                numView[docName.indexOf(doc)] = Integer.parseInt(singleLine[1]);
-            }
-        }
-        br.close();
-
-        x_k = sortRank(pageRank);
-        y_k = sortRank(numView);
-
-        System.out.println(docName);
-        System.out.println(Arrays.toString(pageRank));
-        System.out.println(Arrays.toString(numView));
-        System.out.println(Arrays.toString(x_k));
-        System.out.println(Arrays.toString(y_k));
-
-        int z = 0;
-        for (int i = 0; i < x_k.length; i++)
-        {
-            z += x_k[i];
-        }
-        z /= x_k.length;
-
-        double top = 0, bottom1 = 0, bottom2 = 0;
-        for (int i = 0; i < x_k.length; i++)
-        {
-            top += (x_k[i]-z)*(y_k[i]-z);
-            bottom1 += Math.pow((x_k[i]-z),2);
-            bottom2 += Math.pow((y_k[i]-z),2);
-        }
-        double rho = top/(bottom1*bottom2);
-        System.out.print(rho);
+  public static void main(String[] args) throws Exception {
+    if(args == null || args.length != 2) {
+      System.err.println("Fatal error: needs 2 parameters");
+      System.exit(-1);
     }
 
-    public static double[] sortRank(Double[] data) throws Exception {
-        int n = data.length;
-        int numDuplicates;
-        double rankEle, rankNum = 1;
-        double[] rank = new double[n];
-        Double[] tempData= new Double[n];
-        System.arraycopy(data, 0, tempData, 0, n);
-        tempData = removeDuplicates(tempData);
-        Arrays.sort(tempData, Collections.reverseOrder());
+    pathToPagerank = args[0];
+    pathToNumviews = args[1];
 
-        for (int i = 0; i < tempData.length; i++)
-        {
-            numDuplicates = 0;
-            for (int j = 0; j < n; j++)
-                if (data[j]==tempData[i])
-                    numDuplicates++;
-            rankEle = rankNum + 0.5*(numDuplicates-1);
-            for (int j = 0; j < n; j++)
-            {
-                if (data[j]==tempData[i])
-                    rank[j] = rankEle;
-            }
-            rankNum = rankNum + numDuplicates;
+    // count the number of documents we are considering
+    br = new BufferedReader(new FileReader(pathToPagerank));
+    String line = null;
+    int n = 0;
+    while((line = br.readLine()) != null) {
+      n++;
+    }
+    br.close();
+
+    String[] singleLine;
+    String doc;
+    float[] pageRank = new float[n];
+    int[] numView = new int[n];
+    float[] x_k, y_k;
+    List<String> docName = Lists.newArrayList();
+
+    br = new BufferedReader(new FileReader(pathToPagerank));
+    for(int i = 0; i < n; i++) {
+      line = br.readLine();
+      singleLine = line.split("\\s+");
+      docName.add(singleLine[0]);
+      pageRank[i] = Float.parseFloat(singleLine[1]);
+    }
+    br.close();
+
+    br = new BufferedReader(new FileReader(pathToNumviews));
+    while((line = br.readLine()) != null) {
+      singleLine = line.split("\\s+");
+      doc = singleLine[0];
+      if (docName.contains(doc)) {
+        numView[docName.indexOf(doc)] = Integer.parseInt(singleLine[1]);
+      }
+    }
+    br.close();
+
+    x_k = sortRank(pageRank);
+    y_k = sortRank(numView);
+
+    System.out.println(docName);
+    System.out.println(Arrays.toString(pageRank));
+    System.out.println(Arrays.toString(numView));
+    System.out.println(Arrays.toString(x_k));
+    System.out.println(Arrays.toString(y_k));
+
+    int z = 0;
+    for(int i = 0; i < x_k.length; i++) {
+      z += x_k[i];
+    }
+    z /= x_k.length;
+
+    double top = 0, bottom1 = 0, bottom2 = 0;
+    for (int i = 0; i < x_k.length; i++) {
+      top     += (x_k[i] - z) * (y_k[i] - z);
+      bottom1 += (x_k[i] - z) * (x_k[i] - z);
+      bottom2 += (y_k[i] - z) * (y_k[i] - z);
+    }
+    double rho = top / (bottom1 * bottom2);
+    System.out.print(rho);
+  }
+
+  public static float[] sortRank(float[] data) throws Exception {
+    int n = data.length;
+    int numDuplicates;
+    float rankEle, rankNum = 1.0f;
+    float[] rank = new float[n];
+    Float[] tempData = new Float[n];
+    for(int i = 0; i < n; i++) {
+      tempData[i] = data[i];
+    }
+    tempData = removeDuplicates(tempData);
+    Arrays.sort(tempData, Collections.reverseOrder());
+
+    for(int i = 0; i < tempData.length; i++) {
+      numDuplicates = 0;
+      for(int j = 0; j < n; j++) {
+        if(data[j] == tempData[i]) {
+          numDuplicates++;
         }
-        return rank;
-    }
-
-    public static double[] sortRank(Integer[] data) throws Exception {
-        int n = data.length;
-        int numDuplicates;
-        double rankEle, rankNum = 1;
-        double[] rank = new double[n];
-        Integer[] tempData= new Integer[n];
-        System.arraycopy(data, 0, tempData, 0, n);
-        tempData = removeDuplicates(tempData);
-        Arrays.sort(tempData, Collections.reverseOrder());
-
-        for (int i = 0; i < tempData.length; i++)
-        {
-            numDuplicates = 0;
-            for (int j = 0; j < n; j++)
-                if (data[j]==tempData[i])
-                    numDuplicates++;
-            rankEle = rankNum + 0.5*(numDuplicates-1);
-            for (int j = 0; j < n; j++)
-            {
-                if (data[j]==tempData[i])
-                    rank[j] = rankEle;
-            }
-            rankNum = rankNum + numDuplicates;
+      }
+      rankEle = rankNum + 0.5f * (numDuplicates - 1);
+      for(int j = 0; j < n; j++) {
+        if(data[j]==tempData[i]) {
+          rank[j] = rankEle;
         }
-        return rank;
+      }
+      rankNum = rankNum + numDuplicates;
     }
+    return rank;
+  }
 
-    public static Integer[] removeDuplicates(Integer[] arr) {
-        return new HashSet<Integer>(Arrays.asList(arr)).toArray(new Integer[0]);
+  public static float[] sortRank(int[] data) throws Exception {
+    int n = data.length;
+    int numDuplicates;
+    float rankEle, rankNum = 1;
+    float[] rank = new float[n];
+    Integer[] tempData= new Integer[n];
+    for(int i = 0; i < n; i++) {
+      tempData[i] = data[i];
     }
+    tempData = removeDuplicates(tempData);
+    Arrays.sort(tempData, Collections.reverseOrder());
 
-    public static Double[] removeDuplicates(Double[] arr) {
-        return new HashSet<Double>(Arrays.asList(arr)).toArray(new Double[0]);
+    for (int i = 0; i < tempData.length; i++) {
+      numDuplicates = 0;
+      for (int j = 0; j < n; j++) {
+        if (data[j] == tempData[i]) {
+          numDuplicates++;
+        }
+      }
+      rankEle = rankNum + 0.5f * (numDuplicates - 1);
+      for (int j = 0; j < n; j++) {
+        if (data[j] == tempData[i]) {
+          rank[j] = rankEle;
+        }
+      }
+      rankNum = rankNum + numDuplicates;
     }
+    return rank;
+  }
+
+  public static Integer[] removeDuplicates(Integer[] arr) {
+    return new HashSet<Integer>(Arrays.asList(arr)).toArray(new Integer[0]);
+  }
+
+  public static Float[] removeDuplicates(Float[] arr) {
+    return new HashSet<Float>(Arrays.asList(arr)).toArray(new Float[0]);
+  }
 }
