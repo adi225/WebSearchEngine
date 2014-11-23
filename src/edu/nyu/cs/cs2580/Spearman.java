215b8ef7785977f4b461a343269ac194a3ea7fd1
adi225 <adi225@nyu.edu>
2014-11-22 23:59:28 -0500

    Refactored Spearman to use data structures and check for assumptions.

--- src/edu/nyu/cs/cs2580/Spearman.java
+++ src/edu/nyu/cs/cs2580/Spearman.java
@@ -1,6 +1,9 @@
 package edu.nyu.cs.cs2580;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import static com.google.common.base.Preconditions.*;
 
 import java.io.*;
 import java.util.*;
@@ -19,152 +22,82 @@ public class Spearman
 {
   static String pathToPagerank = null;
   static String pathToNumviews = null;
-  static BufferedReader br = null;
+  static Scanner scanner = null;
 
   public static void main(String[] args) throws Exception {
-    if(args == null || args.length != 2) {
-      System.err.println("Fatal error: needs 2 parameters");
-      System.exit(-1);
-    }
+    checkArgument(checkNotNull(args).length == 2, "Fatal error: needs 2 parameters.");
 
     pathToPagerank = args[0];
     pathToNumviews = args[1];
 
-    // count the number of documents we are considering
-    br = new BufferedReader(new FileReader(pathToPagerank));
-    String line = null;
-    int n = 0;
-    while((line = br.readLine()) != null) {
-      n++;
-    }
-    br.close();
-
     // collect PageRank and Numviews data
-    String[] singleLine;
-    String doc;
-    float[] pageRank = new float[n];
-    int[] numView = new int[n];
-    float[] x_k, y_k;
-    List<String> docName = Lists.newArrayList();
+    Map<String, Float> pageRanks = Maps.newHashMap();
+    Map<String, Integer> numViews = Maps.newHashMap();
 
-    br = new BufferedReader(new FileReader(pathToPagerank));
-    for(int i = 0; i < n; i++) {
-      line = br.readLine();
-      singleLine = line.split("\\s+");
-      docName.add(singleLine[0]);
-      pageRank[i] = Float.parseFloat(singleLine[1]);
+    scanner = new Scanner(new BufferedReader(new FileReader(pathToPagerank)));
+    while(scanner.hasNextLine()) {
+      String[] line = scanner.nextLine().split("\\s+");
+      checkState(line.length == 2);
+      pageRanks.put(line[0], Float.parseFloat(line[1]));
     }
-    br.close();
+    scanner.close();
 
-    br = new BufferedReader(new FileReader(pathToNumviews));
-    while((line = br.readLine()) != null) {
-      singleLine = line.split("\\s+");
-      doc = singleLine[0];
-      if (docName.contains(doc)) {
-        numView[docName.indexOf(doc)] = Integer.parseInt(singleLine[1]);
+    scanner = new Scanner(new BufferedReader(new FileReader(pathToNumviews)));
+    while(scanner.hasNextLine()) {
+      String[] line = scanner.nextLine().split("\\s+");
+      checkState(line.length == 2);
+      if(pageRanks.containsKey(line[0])) {
+        numViews.put(line[0], Integer.parseInt(line[1]));
       }
     }
-    br.close();
+    scanner.close();
+
+    // Verify assumption that pageRanks pages and numViews pages are the same.
+    checkState(pageRanks.size() == numViews.size(), "Different number of pages for numviews and pagerank.");
+    for(String page : pageRanks.keySet()) {
+      checkState(numViews.containsKey(page), "Page " + page + " does not have numViews.");
+    }
 
     // assign rank for each document
-    x_k = assignRank(pageRank);
-    y_k = assignRank(numView);
-
-    System.out.println(docName);
-    System.out.println(Arrays.toString(pageRank));
-    System.out.println(Arrays.toString(numView));
-    System.out.println(Arrays.toString(x_k));
-    System.out.println(Arrays.toString(y_k));
+    List<Float> x_k = assignRank(pageRanks);
+    List<Float> y_k = assignRank(numViews);
 
     // calculate Spearman coefficient
-    int z = 0;
-    for(int i = 0; i < x_k.length; i++) {
-      z += x_k[i];
+    float z = 0;
+    for(int i = 0; i < x_k.size(); i++) {
+      z += x_k.get(i);
     }
-    z /= x_k.length;
+    z /= x_k.size();
 
-    double top = 0, bottom1 = 0, bottom2 = 0;
-    for (int i = 0; i < x_k.length; i++) {
-      top     += (x_k[i] - z) * (y_k[i] - z);
-      bottom1 += (x_k[i] - z) * (x_k[i] - z);
-      bottom2 += (y_k[i] - z) * (y_k[i] - z);
+    float top = 0, bottom1 = 0, bottom2 = 0;
+    for(int i = 0; i < x_k.size(); i++) {
+      top     += (x_k.get(i) - z) * (y_k.get(i) - z);
+      bottom1 += (x_k.get(i) - z) * (x_k.get(i) - z);
+      bottom2 += (y_k.get(i) - z) * (y_k.get(i) - z);
     }
     double rho = top / Math.sqrt(bottom1 * bottom2);
-    System.out.print(rho);
+    System.out.println(rho);
   }
 
-  // assigns rank values of each element in score
-  public static float[] assignRank(float[] score) throws Exception {
-    int n = score.length;
-    int numDuplicates;
-    float rankValue, rankNum = 1.0f;
-    float[] rank = new float[n];
-    Float[] descendingRank = new Float[n];
-    for(int i = 0; i < n; i++) {
-      descendingRank[i] = score[i];
-    }
-    descendingRank = removeDuplicates(descendingRank);
-    Arrays.sort(descendingRank, Collections.reverseOrder());
+  public static <V extends Comparable<V>> List<Float> assignRank(Map<String, V> values) {
+    List<Float> ranks = Lists.newArrayList();
+    List<Map.Entry<String, V>> sortedValues = Utils.sortByValues(values, true);
+    int actualRank = 1;
 
-    for(int i = 0; i < descendingRank.length; i++) {
-      // count the number of duplicates
-      numDuplicates = 0;
-      for(int j = 0; j < n; j++) {
-        if(score[j] == descendingRank[i]) {
-          numDuplicates++;
-        }
+    for(int i = 0; i < sortedValues.size(); i++) {
+      float calculatedRank = actualRank++;
+      int numDuplicates = 1;
+      while(i+1 < sortedValues.size()
+            && sortedValues.get(i+1).getValue().compareTo(sortedValues.get(i).getValue()) == 0) {
+        calculatedRank += actualRank++;
+        numDuplicates++;
+        i++;
       }
-      // assigns rank values
-      rankValue = rankNum + 0.5f * (numDuplicates - 1);
-      for(int j = 0; j < n; j++) {
-        if(score[j]==descendingRank[i]) {
-          rank[j] = rankValue;
-        }
+      for(int j = 0; j < numDuplicates; j++) {
+        ranks.add(calculatedRank / numDuplicates);
       }
-      rankNum = rankNum + numDuplicates;
     }
-    return rank;
-  }
-
-  // assigns rank values of each element in score
-  public static float[] assignRank(int[] score) throws Exception {
-    int n = score.length;
-    int numDuplicates;
-    float rankValue, rankNum = 1;
-    float[] rank = new float[n];
-    Integer[] descendingRank= new Integer[n];
-    for(int i = 0; i < n; i++) {
-      descendingRank[i] = score[i];
-    }
-    descendingRank = removeDuplicates(descendingRank);
-    Arrays.sort(descendingRank, Collections.reverseOrder());
-
-    for (int i = 0; i < descendingRank.length; i++) {
-      // count the number of duplicates
-      numDuplicates = 0;
-      for (int j = 0; j < n; j++) {
-        if (score[j] == descendingRank[i]) {
-          numDuplicates++;
-        }
-      }
-
-      // assigns rank values
-      rankValue = rankNum + 0.5f * (numDuplicates - 1);
-      for (int j = 0; j < n; j++) {
-        if (score[j] == descendingRank[i]) {
-          rank[j] = rankValue;
-        }
-      }
-      rankNum = rankNum + numDuplicates;
-    }
-    return rank;
-  }
-
-  public static Integer[] removeDuplicates(Integer[] arr) {
-    return new HashSet<Integer>(Arrays.asList(arr)).toArray(new Integer[0]);
-  }
-
-  public static Float[] removeDuplicates(Float[] arr) {
-    return new HashSet<Float>(Arrays.asList(arr)).toArray(new Float[0]);
+    checkState(values.size() == ranks.size(), "Ranks incorrectly generated.");
+    return ranks;
   }
 }
